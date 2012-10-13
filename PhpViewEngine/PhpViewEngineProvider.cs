2e179bac064f8dca43bf57b93280a2def6f0e377
Benedek Farkas <none@none>
2012-10-13 10:28:43 +0200

    Adding files for implementing PHP View Engine

---
+++ PhpViewEngine/PhpViewEngineProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Orchard.Mvc.ViewEngines;
+using Orchard.DisplayManagement.Descriptors.ShapeTemplateStrategy;
+
+namespace OrchardHUN.Scripting.Php.PhpViewEngine
+{
+    public class PhpViewEngineProvider : IViewEngineProvider, IShapeTemplateViewEngine
+    {
+        public IViewEngine CreateThemeViewEngine(CreateThemeViewEngineParams parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IViewEngine CreateModulesViewEngine(CreateModulesViewEngineParams parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IViewEngine CreateBareViewEngine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> DetectTemplateFileNames(IEnumerable<string> fileNames)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
