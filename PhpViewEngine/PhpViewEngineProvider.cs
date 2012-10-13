be0094ab001df382d2f12e8962870bcb72d7927d
Benedek Farkas <none@none>
2012-10-13 10:44:00 +0200

    Project file saved

--- PhpViewEngine/PhpViewEngineProvider.cs
+++ PhpViewEngine/PhpViewEngineProvider.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using Orchard.Mvc.ViewEngines;
 using Orchard.DisplayManagement.Descriptors.ShapeTemplateStrategy;
+using Orchard.Mvc.ViewEngines;
 
 namespace OrchardHUN.Scripting.Php.PhpViewEngine
 {
