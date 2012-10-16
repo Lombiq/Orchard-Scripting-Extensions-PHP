c06f6244fc1cca8a7a8ffee1a6da751c43dc4f0a
Piedone <piedone@pyrocenter.hu>
2012-10-16 13:35:20 +0200

    Moving testbed to core module

--- ViewModels/TestBedViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OrchardHUN.Scripting.Php.ViewModels
-{
-    public class TestBedViewModel
-    {
-        public string Code { get; set; }
-        public string Output { get; set; }
-    }
-}
