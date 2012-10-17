0a3d1e9f805628c47cd6cc39c6beb92b67d3209b
Piedone <piedone@pyrocenter.hu>
2012-10-17 21:30:07 +0200

    Adapting to content item scripts
    Also updating to VS 2012 project file

---
+++ Drivers/ScriptPartDriver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OrchardHUN.Scripting.Models;
+using Orchard.ContentManagement.Drivers;
+using Orchard.ContentManagement;
+
+namespace OrchardHUN.Scripting.Php.Drivers
+{
+    public class ScriptPartDriver : ContentPartDriver<ScriptPart>
+    {
+        protected override string Prefix
+        {
+            get { return "OrchardHUN.Scripting.ScriptPart"; }
+        }
+
+        protected override DriverResult Editor(ScriptPart part, dynamic shapeHelper)
+        {
+            return ContentShape("Parts_ScriptPhp_Edit",
+                    () => shapeHelper.EditorTemplate(
+                            TemplateName: "Parts.ScriptPhp",
+                            Model: part,
+                            Prefix: Prefix));
+        }
+
+        protected override DriverResult Editor(ScriptPart part, IUpdateModel updater, dynamic shapeHelper)
+        {
+            return Editor(part, shapeHelper);
+        }
+    }
+}
